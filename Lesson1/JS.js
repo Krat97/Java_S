b46db0c6e185ed45b419524d6c1fe0717e1c935f
Krat97 <ol.maksymiuk@gmail.com>
2023-05-09 22:50:35 +0300

    Initial commit

---
+++ Lesson1/JS.js
@@ -0,0 +1,37 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// Додаткове для тих хто цікавився prompt`oм
+//  - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// let a = 'hello'
+// console.log(a);
+// let b = 'owu'
+// console.log(b);
+// let c = 'com'
+// console.log(c);
+// let d = 'ua'
+// console.log(d);
+// ///////////////////////////////////////////////////
+// let firstName = `Oleh`;
+// let lastName = `Maksimyk`;
+// let middleName = `Ivanovich`;
+// let person = `${firstName} ${lastName} ${middleName}`
+// console.log(person);
+///////////////////////////////////////////////////////
+// let a = 100; let b = '100'; let c = true;
+// console.log(`${a}`+` = `+ typeof a);
+// console.log(`${b}`+` = `+ typeof b);
+// console.log(`${c}`+` = `+ typeof c);
+///////////////////////////////////////////////////////
+// let name = prompt(`Імя?`)
+// console.log(name);
+// let fname = prompt(`По-Батькові?`)
+// console.log(fname);
+// let age = +prompt(`вік?`)
+// console.log(age);
+
